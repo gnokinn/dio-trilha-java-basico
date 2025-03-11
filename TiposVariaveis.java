b26e7beb594402da268efc017522be4cdbd93076
Gnokinn <vinicius.santos@dcx.ufpb.br>
2025-03-10 21:45:16 -0300

    Atualizando...

--- TiposVariaveis.java
+++ TiposVariaveis.java
@@ -1,2 +1,25 @@
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        //tipos primitivos
+
+
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        /* forma errada:
+        short numeroCurto2 = numeroNormal; */
+        short numeroCurto2 = (short) numeroNormal; //forma correta
+
+        int numero = 5;
+
+        numero = 10; //variável sofre alteração
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14; //constante que nunca pode ser auterada
+
+
+    }
 }
